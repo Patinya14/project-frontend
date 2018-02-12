16c9e271a9bdc140ab047a0ae4a4289718497bcb
Patinya14 <57160342@go.buu.ac.th>
2018-02-12 17:26:33 +0700

    first add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PersonalComponent } from './add/personal/personal.component';
 
+
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PersonalComponent,
+    
+   
   ],
   imports: [
     BrowserModule
