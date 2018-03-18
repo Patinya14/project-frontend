8aff6f0b693ed6cbc5ef7a85025ab7dff71fca79
Patinya14 <57160342@go.buu.ac.th>
2018-03-18 18:38:07 +0700

    MMMM

---
+++ src/app/service/certificate.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ApiService} from './api.service';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class CertificateService {
+    constructor (
+        private apiService: ApiService
+    ){ }
+    getCer(): Observable<any>{
+        return this.apiService.get('certificate');
+    }
+    addCer(data): Observable<any>{
+        return this.apiService.post('certificate',data);
+    }
+    updateCer(id: string , data): Observable<any>{
+        return this.apiService.put('certificate'+ id,data);
+    }
+    deleteCer(id: string): Observable<any>{
+        return this.apiService.delete('certificate'+id);
+    }
+
+
+
+
+
+}
---
+++ src/app/service/general.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ApiService } from './api.service';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class GeneralService {
+  constructor(
+    private apiService: ApiService
+  ) { }
+  getGen(): Observable<any> {
+    return this.apiService.get(`general`);
+  }
+  addGen(data): Observable<any> {
+    return this.apiService.post('General', data);
+  }
+  updateGen(id: string, data): Observable<any> {
+    return this.apiService.put('genreal/' + id, data);
+  }
+  deleteGen(id: string): Observable<any> {
+    return this.apiService.delete('general/' + id);
+  }
+  
+}
